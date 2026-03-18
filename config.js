a08db85924d01d63d85073845587fdb59c6f7dbf
jrsteve.eth <245251852+jrsteve900@users.noreply.github.com>
2026-03-18 22:43:41 +0000

    feat:  Stealth Portal - Production Ready

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+        NETWORK: {
+                    name: 'Base',
+                            chainId: '0x2105',
+                                    rpc: 'https://mainnet.base.org',
+                                            explorer: 'https://basescan.org'
+        },
+            TOKEN: {
+                        address: '0x7A49713Fc8D91B1B33d79aAa2098b5b6Ba536214',
+                                symbol: 'TAMU'
+            }
+};
